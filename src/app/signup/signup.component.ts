a96bb48e922331dd424697fde4acd4b96ce542d9
JHades <jhades.dev@gmail.com>
2017-07-18 15:04:53 +0200

    Angular Security course

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css', '../common/forms.css']
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+    form:FormGroup;
 
-  ngOnInit() {
-  }
+    constructor(private fb: FormBuilder) {
+
+        this.form = this.fb.group({
+            email: ['',Validators.required],
+            password: ['',Validators.required],
+            confirm: ['',Validators.required]
+        });
+
+
+    }
+
+    ngOnInit() {
+
+    }
+
+
+    signUp() {
+        const val = this.form.value;
+
+        //TODO
+
+    }
 
 }
