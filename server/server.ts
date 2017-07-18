786e7f13e629de64680ba4e0f689a8146aa00e66
JHades <jhades.dev@gmail.com>
2017-07-18 11:06:32 +0200

    Angular Security course

--- server/server.ts
+++ server/server.ts
@@ -2,16 +2,27 @@
 
 import * as express from 'express';
 import {Application} from "express";
-
+import * as fs from 'fs';
+import * as https from 'https';
 
 const app: Application = express();
 
 
+const server = https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}, app);
 
 
-
-const server = app.listen(9000, () => {
-    console.log("Server running at https://localhost:" + server.address().port);
+app.get('/', function (req, res) {
+    res.header('Content-type', 'text/html');
+    return res.end('<h1>Hello, Secure World!</h1>');
 });
+
+
+server.listen(55555, () => console.log("Server running at https://localhost:" + server.address().port));
+
+
+
 
 
