e6f97bb401c0213fadfcc4b47fc6d7c2195a688c
Your Name <you@example.com>
2017-07-22 15:31:08 +0200

    angular security course

--- demos/hash.js
+++ demos/hash.js
@@ -2,7 +2,13 @@
 
 var crypto = require('crypto');
 
-var password = 'monkey';
+var password = '123456789';
+
+var aliceSalt = 1;
+
+var bobSalt = 2;
+
+
 
 // we will use another hash other SHA-256 during the course, this is just for demo purposes
 var hash = crypto.createHash('sha256').update(password).digest('hex');
