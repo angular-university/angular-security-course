05aeb170e532195ede379dffddc1f742e063e0ce
Your Name <you@example.com>
2017-07-22 14:08:55 +0200

    angular security course

--- demos/hash-salt.js
+++ demos/hash-salt.js
@@ -1,2 +1,20 @@
 
 
+var crypto = require('crypto');
+
+
+var password = "monkey";
+
+
+crypto.randomBytes(256, function(err, salt) {
+
+    crypto.pbkdf2(password, salt, 100000, 512, 'sha256',
+        function(err, hash) {
+
+            console.log("The result of hashing " + password + " is:\n\n" +
+                hash.toString('hex') + "\n\n");
+
+        });
+
+
+});
