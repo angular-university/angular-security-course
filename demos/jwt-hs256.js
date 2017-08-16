dc151d1ebfdafb501687c21f754665c57798ad6c
JHades <jhades.dev@gmail.com>
2017-08-16 11:52:04 +0200

    Angular Security course

--- demos/jwt-hs256.js
+++ demos/jwt-hs256.js
@@ -1,2 +1,21 @@
 
-console.log("JWT example");
+console.log(" JWT example using HMAC SHA256");
+
+var jwt = require('jsonwebtoken');
+
+
+
+var secretKey = 'secret-key';
+
+var payload = {
+  email: 'test@mailinator.com'
+};
+
+
+var token = jwt.sign(payload, secretKey, {algorithm: 'HS256' });
+
+
+console.log(token);
+
+
+
