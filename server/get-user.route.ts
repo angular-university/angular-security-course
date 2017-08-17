1e378c08288870ac9841c95201a4b9eda81ff630
Your Name <you@example.com>
2017-08-17 15:19:59 +0200

    angular security course

--- server/get-user.route.ts
+++ server/get-user.route.ts
@@ -10,7 +10,7 @@ export function getUser(req:Request, res:Response) {
     const user = db.findUserById(req["userId"]);
 
     if (user) {
-        res.status(200).json(user);
+        res.status(200).json({email:user.email});
     }
     else {
         res.sendStatus(204);
