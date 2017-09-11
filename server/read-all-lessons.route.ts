490cb1f247dad128f603a1bd97f597d75cf98ee2
JHades <jhades.dev@gmail.com>
2017-09-11 14:14:07 +0200

    Angular Security course

--- server/read-all-lessons.route.ts
+++ server/read-all-lessons.route.ts
@@ -4,8 +4,6 @@ import {db} from "./database";
 
 export function readAllLessons(req, res) {
 
-    console.log("User is reading lessons data", req.user);
-
     res.status(200).json({lessons:db.readAllLessons()});
 
 }
