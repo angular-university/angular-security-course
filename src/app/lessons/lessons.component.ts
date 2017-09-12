2458bf866381e6d0d1d117ad53b1c1c33f3c0e78
Your Name <you@example.com>
2017-09-12 11:24:17 +0200

    angular security course

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {LessonsService} from "../services/lessons.service";
 import {Observable} from "rxjs/Observable";
 import {Lesson} from "../model/lesson";
@@ -8,7 +8,7 @@ import {Lesson} from "../model/lesson";
     templateUrl: './lessons.component.html',
     styleUrls: ['./lessons.component.css']
 })
-export class LessonsComponent implements OnInit {
+export class LessonsComponent implements OnInit, AfterViewInit {
 
     lessons$: Observable<Lesson[]>;
 
@@ -17,7 +17,36 @@ export class LessonsComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.lessons$ = this.lessonsService.loadAllLessons().catch(err => Observable.of([]));
+
+        this.lessons$ = this.lessonsService.loadAllLessons()
+            .catch(err => Observable.of([]));
+    }
+
+
+    ngAfterViewInit() {
+
+        this.lessons$.subscribe( lessons => {
+
+            const container = document.getElementById('lessons');
+
+            lessons.forEach(lesson => {
+
+                const lessonDiv = document.createElement("div");
+
+                lessonDiv.className = "lesson-detail card card-strong";
+
+                const h3 = document.createElement("h3");
+                h3.innerText = lesson.description;
+
+                lessonDiv.appendChild(h3);
+
+                container.appendChild(lessonDiv);
+
+
+            });
+
+        });
+
     }
 
 
