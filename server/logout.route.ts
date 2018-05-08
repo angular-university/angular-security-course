77c7beb5ba3ffa0b366aea1121599d736d13ad78
JHades <jhades.dev@gmail.com>
2018-05-08 11:36:21 +0200

    Course updated to Angular 6

--- server/logout.route.ts
+++ server/logout.route.ts
@@ -10,5 +10,5 @@ export function logout(req: Request, res: Response) {
 
     res.clearCookie("XSRF-TOKEN");
 
-    res.sendStatus(200);
+    res.status(200).json({message: 'Logout Successful'});
 }
