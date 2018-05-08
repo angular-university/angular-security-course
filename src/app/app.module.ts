f9e8f8d800264554dc2c85a4e1954be8fc649a46
JHades <jhades.dev@gmail.com>
2018-05-08 10:49:32 +0200

    Course updated to Angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,10 @@ import {routesConfig} from "./routes.config";
 import {LessonsService} from "./services/lessons.service";
 import {ReactiveFormsModule} from "@angular/forms";
 
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/shareReplay';
-import 'rxjs/add/operator/do';
+
+
+
+
 
 import {AuthService} from "./services/auth.service";
 
